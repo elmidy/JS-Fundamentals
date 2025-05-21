ba5b12309136f39743645ee4a92b7073d906321e
Muhammed Elmidy <162920636+elmidy@users.noreply.github.com>
2025-05-21 19:47:14 +0300

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,6 +1,12 @@
 let i = 1;
 if (i=1){
-    console.log('Arguments found');
-    console.log('Argument found');
     console.log('No argument');
+}
+i = 0;
+if (i=0){
+    console.log('Argument found');
+}
+i = 2; 
+if (i=2){
+    console.log('Arguments found');
 }
