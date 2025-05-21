194e231ccd9f24015b7703fd5e42bffb2b3d2ce8
Muhammed Elmidy <162920636+elmidy@users.noreply.github.com>
2025-05-21 20:22:47 +0300

    Create 8-square.js

---
+++ 8-square.js
@@ -0,0 +1,9 @@
+const arg = process.argv[2];
+const num = Number.parseInt(arg, 10);
+if (Number.isNaN(num)) {
+    console.log('Missing size');
+} else {
+    for (let i = 0; i < num; i++) {
+        console.log('x'.repeat(num));
+    }
+}
