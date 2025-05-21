af260f57e164d910810a67ab11bdb30c9b032fee
Muhammed Elmidy <162920636+elmidy@users.noreply.github.com>
2025-05-21 20:10:01 +0300

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,5 +1,2 @@
-const func1 = (name1, name2) => `${name1} is ${name2}`;
-console.log(func1('Python','fun'));
-console.log(func1('HBTN'));
-console.log(func1());
-
+const args = process.argv.slice(2);
+console.log(`${args[0]} is ${args[1]}`);
