6a47a69a8dde42dc36f7ccb23635c81d086747b1
Muhammed Elmidy <162920636+elmidy@users.noreply.github.com>
2025-05-21 20:28:26 +0300

    Update 9-add.js

--- 9-add.js
+++ 9-add.js
@@ -1,10 +1,15 @@
+function add(a, b) {
+    return a + b;
+}
+// This script takes two command line arguments, converts them to numbers, and adds them together.
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
 const num1 = Number.parseInt(arg1, 10);
 const num2 = Number.parseInt(arg2, 10);
+// If either argument is not a number, it prints 'NaN'.
+// Otherwise, it prints the sum of the two numbers.
 if (Number.isNaN(num1) || Number.isNaN(num2)) {
-    console.log('NAN');
+    console.log('NaN');
 } else {
-    const sum = num1 + num2;
-    console.log(sum);
+    console.log(add(num1, num2));
 }
